91407cf5243d71cec60fcef69efb36cff9ac0e72
Chaewoon-kim <149470542+Chaewoon-kim@users.noreply.github.com>
2025-04-27 13:38:01 +0900

    Time: 4 ms (57.7%), Space: 45.3 MB (18.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0 ; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length ; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i , j};
-                }
-            }
+      //HashMap으로 시간복잡도 개선
+      Map<Integer , Integer> numsMap = new HashMap<>();
+
+      for(int i = 0 ; i < nums.length ; i++){
+        numsMap.put(nums[i] , i); 
+      }
+
+      //target - first value = Second value
+      for(int i = 0 ; i < nums.length; i++){
+        if(numsMap.containsKey(target - nums[i]) && i != numsMap.get(target - nums[i])){
+            return new int [] {i , numsMap.get(target - nums[i])};
         }
-        return null;
+      }
+
+      return null;
     }
 }
